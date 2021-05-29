d7120f81358e9b6906d28c075744f2447e0be848
Adam Skynew <cronos2277@gmail.com>
2021-05-28 21:52:55 -0300

    Criando estrutura básica para estudo do C#

---
+++ C#/Fundamentos/Primeiro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fundamentos
+{
+    class Primeiro
+    {
+        public static void Executar()
+        {
+            Console.Write("Meu Olá mundo!");
+        }
+    }
+}
---
+++ C#/Fundamentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fundamentos
+{
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Ola Mundo - Fundamentos", Primeiro.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
